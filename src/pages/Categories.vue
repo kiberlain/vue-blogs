6b8659c2de49c2da22e5bb12948540112e0a7fce
kurnickiy <yeXvskU2Q5E5rsovz56b>
2022-08-03 09:14:38 +0300

    структура проекта

---
+++ src/pages/Categories.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Все посты</h1>
+</template>
---
+++ src/pages/info-posts/InfoPosts.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Все посты - Infoposts.vue</h1>
+
+  {{ posts }}
+</template>
+
+<script>
+import ApiService from "../../service/ApiService";
+import { supabase } from "../../supabase"
+
+
+export default {
+  
+  setup() {
+    ApiService.getAllPosts();
+  }
+
+};
+</script>
